a4ef7695045fdf53f70db7b92552f7d9d89bb855
ibavca2 <bavc051977@mail.ru>
2015-03-25 23:47:03 +0600

    Создал новый проект Console NHibernate

---
+++ ConsoleA1/HibernateHelper.cs
@@ -0,0 +1,39 @@
+﻿using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate;
+using NHibernate.Tool.hbm2ddl;
+
+namespace ConsoleA1
+{
+    internal class HibernateHelper
+    {
+        private static ISessionFactory _sessionFactory;
+        private static ISessionFactory SessionFactory
+        {
+            get
+            {
+                if (_sessionFactory == null)
+                    InitializerSessionFactory();
+                return _sessionFactory;
+            }
+        }
+
+        private static void InitializerSessionFactory()
+        {
+            _sessionFactory = Fluently.Configure()
+                .Database(MsSqlConfiguration.MsSql2012.ConnectionString(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=E:\Белоногов\ConsoleA1\ConsoleA1\myLocal.mdf;Integrated Security=True")
+                .ShowSql())
+                 .Mappings(m =>
+                    m.FluentMappings
+                    .AddFromAssemblyOf<Car>())
+                .ExposeConfiguration(cfg => new SchemaExport(cfg)
+                .Create(true, true))
+                .BuildSessionFactory();
+        }
+
+        public static ISession OpenSession()
+        {
+            return SessionFactory.OpenSession();
+        }
+    }
+}
---
+++ ConsoleA1/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentNHibernate;
+using FluentNHibernate.Mapping;
+
+namespace ConsoleA1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var session = HibernateHelper.OpenSession())
+            {
+                using (var transaction = session.BeginTransaction())
+                {
+                    var lada = new Make
+                    {
+                        Name = "Lada"
+                    };
+                    var ladaModel = new Model
+                    {
+                        Name = "2114",
+                        Make = lada
+                    };
+                    var car = new Car
+                    {
+                        Make = lada,
+                        Model = ladaModel,
+                        Title = "Моя машина"
+                    };
+                    session.Save(car);
+                    transaction.Commit();
+                    Console.WriteLine("Машина создана: " + car.Title);
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+    public class Make
+    {
+        public virtual int Id { get; set; }
+        public virtual string Name { get; set; }
+    }
+
+    public class Model
+    {
+        public virtual int Id { get; set; }
+        public virtual string Name { get; set; }
+        public virtual Make Make { get; set; }
+    }
+    public class Car
+    {
+        public virtual int Id { get; set; }
+        public virtual string Title { get; set; }
+        public virtual Make Make { get; set; }
+        public virtual Model Model { get; set; }
+    }
+
+    public class MakeMap : ClassMap<Make>
+    {
+        public MakeMap()
+        {
+            Id(x => x.Id);
+            Map(x => x.Name);
+        }
+    }
+    public class ModelMap : ClassMap<Model>
+    {
+        public ModelMap()
+        {
+            Id(x => x.Id);
+            Map(x => x.Name);
+            References(x => x.Make).Cascade.All();
+        }
+    }
+    public class CarMap : ClassMap<Car>
+    {
+        public CarMap()
+        {
+            Id(x => x.Id);
+            Map(x => x.Title);
+            References(x => x.Make).Cascade.All();
+            References(x => x.Model).Cascade.All();
+        }
+    }
+
+}
